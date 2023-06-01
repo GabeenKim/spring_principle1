d3b9f7921ef43a692d1e2cfdfda97bbdf41cf1f9
GabeenKim <rlarkqls5551250@gmail.com>
2023-06-01 23:40:32 +0900

    Test: 무상태로 수정

--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -17,15 +17,16 @@ class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
         //ThreadA: A 사용자 10000원 주문
-        statefulService1.order("userA", 10000);
+        int userAPrice = statefulService1.order("userA", 10000);
         //ThreadB: B 사용자 20000원 주문
-        statefulService2.order("userB", 20000);
+        int userBPrice = statefulService2.order("userB", 20000);
 
         //ThreadA: A 사용자 주문 금액 조회
-        int price = statefulService1.getPrice();
-        System.out.println("price = "+ price);
+        //int price = statefulService1.getPrice();
 
-        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+        System.out.println("price = "+ userAPrice);
+
+       // Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
     }
 
     static class TestConfig{
